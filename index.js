4a76a35c0b35d1093a17e3e4c502b5236448c443
genius-softdev <webdevelopercrane@gmail.com>
2020-05-02 00:59:54 +0700

    Initialize Send Email with Attachment

---
+++ index.js
@@ -0,0 +1,29 @@
+// using Twilio SendGrid's v3 Node.js Library
+// https://github.com/sendgrid/sendgrid-nodejs
+var SENDGRID_API_KEY='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';
+const sgMail = require('@sendgrid/mail');
+
+const fs = require("fs");
+
+const attach = fs.readFileSync(`${__dirname}/attach.csv`).toString("base64");
+
+sgMail.setApiKey(SENDGRID_API_KEY);
+
+const msg = {
+  to: 'webdevelopercrane@gmail.com',
+  from: 'softdeveloper1214@hotmail.com',
+  subject: 'Sending with Twilio SendGrid is Fun',
+  text: 'and easy to do anywhere, even with Node.js',
+  html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+  attachments: [
+    {
+      content: attach,
+      filename: "attach.csv",
+      type: "application/csv",
+      disposition: "attachment"
+    }
+  ]
+};
+sgMail.send(msg)
+.then(response => console.log(response))
+.catch(err => console.log(err));
